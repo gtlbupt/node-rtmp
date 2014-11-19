8ab0ba1f58fe1cfe8b083c4a641ea5d873706893
gaotianlong <gaotianlongbupt@163.com>
2014-11-19 17:10:00 +0800

    refine

--- test/test_empty_server.js
+++ test/test_empty_server.js
@@ -6,8 +6,8 @@ var server = RTMP.createServer(null,  function(nc){
 	nc.accept();
 	nc.on('createStream', function(ns){
 		console.log("createStream");
-		ns.on('publish', function(args){
-			console.log("publish");
+		ns.on('publish', function(args, hasPublish){
+			console.log("publish args[%s] hasPublish[%d]", JSON.stringify(args), hasPublish);
 			ns.acceptPublish(args);	
 		});	
 		ns.on('play', function(){
